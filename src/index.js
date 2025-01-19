a97675527960c30c7227392b2fa8ff1dc65b1f6b
Axel C <axel.croize@hotmail.fr>
2025-01-19 15:24:27 +0100

    gpgpu flowfield in r3F

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import * as THREE from 'three'
+import { render, events, extend } from '@react-three/fiber'
+import './styles.css'
+import App from './App'
+
+extend(THREE)
+
+window.addEventListener('resize', () =>
+  render(<App />, document.querySelector('canvas'), {
+    events,
+    linear: true,
+    camera: { fov: 25, position: [0, 0, 30] },
+    gl: new THREE.WebGLRenderer({
+      canvas: document.querySelector('canvas'),
+      antialias: true,
+      alpha: true
+    })
+  })
+)
+
+window.dispatchEvent(new Event('resize'))
